9265a5f91aed878254343fafd6f9be8dbc71e721
KamilMachowski <45885548+KamilMachowski@users.noreply.github.com>
2020-01-08 11:14:33 +0100

    users and api service added

---
+++ src/app/_model/user.ts
@@ -0,0 +1,9 @@
+export class User{
+    name: string;
+    surname: string;
+    city: string;
+    country: string;
+    email: string;
+    pass: string;
+    admin: boolean;
+}
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getData(search: string) {
+    return this.http.get(`https://pht-api-munonj7kmq-ew.a.run.app/api/chemicals/get?search=${search}`);
+  }
+
+  postData(data: string) {
+    return this.http.post(`https://pht-api-munonj7kmq-ew.a.run.app/api/chemicals/create`, data);
+  }
+
+  putData(data: string) {
+     return this.http.put(`https://pht-api-munonj7kmq-ew.a.run.app/api/chemicals/update`, data);
+  }
+
+  deleteData(id: number) {
+     return this.http.delete(`https://pht-api-munonj7kmq-ew.a.run.app/api/chemicals/delete/${id}`);
+  }
 }
